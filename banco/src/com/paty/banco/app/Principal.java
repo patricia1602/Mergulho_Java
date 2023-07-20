3b5c4ffac488c81d9edf93a0b098617c65756e56
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-07-20 11:25:50 -0300

    Polimorfismo

--- banco/src/com/paty/banco/app/Principal.java
+++ banco/src/com/paty/banco/app/Principal.java
@@ -1,9 +1,6 @@
 package com.paty.banco.app;
 
-import com.paty.banco.modelo.Conta;
-import com.paty.banco.modelo.ContaEspecial;
-import com.paty.banco.modelo.ContaInvestimento;
-import com.paty.banco.modelo.Pessoa;
+import com.paty.banco.modelo.*;
 
 public class Principal {
 
@@ -13,25 +10,25 @@ public class Principal {
         titular1.setNome("Manuela");
         titular1.setDocumento("23452323231");
 
-       Pessoa titular2 = new Pessoa();
-       titular2.setNome("Leonardo");
-       titular2.setDocumento("87656754312");
+        Pessoa titular2 = new Pessoa();
+        titular2.setNome("Leonardo");
+        titular2.setDocumento("87656754312");
 
-        ContaInvestimento minhaConta =  new ContaInvestimento(titular1, 123-5,1234);
-        ContaEspecial suaConta = new ContaEspecial(titular2, 7890-3, 765, 1_000);
+        CaixaEletronico caixaEletronico = new CaixaEletronico();
 
-        //minhaConta.depositar(15_000);
-        //minhaConta.sacar(1_000);
-        //minhaConta.creditarRendimentos(0.8);
+        ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123 - 5, 1234);
+        ContaEspecial suaConta = new ContaEspecial(titular2, 7890 - 3, 765, 1_000);
+
+        minhaConta.depositar(15_000);
+        minhaConta.sacar(1_000);
+        minhaConta.creditarRendimentos(0.8);
 
         suaConta.depositar(15_000);
         suaConta.sacar(15_500);
 
-        //System.out.println("Titular : " + minhaConta.getTitular().getNome());
-        //System.out.println("Saldo : " + minhaConta.getSaldo());
-
-        System.out.println("Titular : " + suaConta.getTitular().getNome());
-        System.out.println("Saldo : " + suaConta.getSaldo());
-
+        caixaEletronico.imprimirSaldo(minhaConta );
+        System.out.println();
+        caixaEletronico.imprimirSaldo(suaConta);
     }
+
 }
